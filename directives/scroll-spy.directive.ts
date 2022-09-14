b6fd41ee37b0e8376537dc5d75c894b54618095b
ehsan lotfi <lotfiniamail@gmail.com>
2022-09-14 15:32:01 +0430

    Create scroll-spy.directive.ts

---
+++ directives/scroll-spy.directive.ts
@@ -0,0 +1,53 @@
+import {
+  Directive,
+  Injectable,
+  Input,
+  EventEmitter,
+  Output,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
+
+@Directive({
+  selector: '[scrollSpy]',
+})
+export class ScrollSpyDirective {
+  @Input() public spiedTags = [];
+  @Output() public sectionChange = new EventEmitter<string>();
+  private currentSection: string;
+
+  inSide: boolean = false;
+
+  constructor(private _el: ElementRef) {}
+
+  @HostListener('mouseenter')
+  mouseenter() {
+    this.inSide = true;
+  }
+
+  @HostListener('mouseleave')
+  mouseover() {
+    this.inSide = false;
+  }
+
+  @HostListener('scroll', ['$event'])
+  onMouseWheel(event: any) {
+    if (!this.inSide) return;
+    let currentSection: string;
+    const children = this._el.nativeElement.children;
+    const scrollTop = event.target.scrollTop;
+    const parentOffset = event.target.offsetTop;
+    for (let i = 0; i < children.length; i++) {
+      const element = children[i];
+      if (this.spiedTags.some((spiedTag) => spiedTag === element.tagName)) {
+        if (element.offsetTop - parentOffset <= scrollTop) {
+          currentSection = element.id;
+        }
+      }
+    }
+    if (currentSection !== this.currentSection) {
+      this.currentSection = currentSection;
+      this.sectionChange.emit(this.currentSection);
+    }
+  }
+}
